f2b2febc3d992f94d5d459e7261ce800f816b958
Shameer Eranhikath <shmr_ekth@yahoo.com>
2020-07-02 14:00:22 +0400

    urls fileis updated

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name='weatherapp'
+app_name = 'weatherapp'
 urlpatterns = [
-    path('', views.index,name='index')
+    path('', views.index, name='index')
 ]
