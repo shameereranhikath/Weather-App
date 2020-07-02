eab70356273c7f55e82f363447302dbc5854ab8c
Shameer Eranhikath <shmr_ekth@yahoo.com>
2020-07-02 22:14:51 +0400

    Modelnamed City is created

--- weatherapp/models.py
+++ weatherapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class City(models.Model):
+    city_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.name
--- weatherapp/views.py
+++ weatherapp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+import requests
 
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hai")
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=19e314a02c78c1106c28037fcf77f454'
+    city = 'Dubai'
+    r = requests.get(url.format(city)).json()
+    # r=url.format(city)
+    # print(r.text)
+    weather_data = {'city': city,
+                    'temperature': r['main']['temp'],
+                    'description': r['weather'][0]['description'],
+                    'icon': r['weather'][0]['icon'],
+                    }
+    print(weather_data)
+    return render(request, 'index.html', {'weather_data': weather_data})
