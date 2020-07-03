3a2bf078013f6ec45ee1b6797a42b760010d1f21
Shameer Eranhikath <shmr_ekth@yahoo.com>
2020-07-03 15:31:24 +0400

    Notification for existing and wrong city was added

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -11,6 +11,8 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=19e314a02c78c1106c28037fcf77f454'
     form = CityCreateForm()
     error_message = ''
+    message = ''
+    message_class = ''
     if request.method == "POST":
         form = CityCreateForm(request.POST)
         if form.is_valid():
@@ -22,31 +24,47 @@ def index(request):
                     form.save()
                 else:
                     error_message = 'Invalid City'
+                    # print(error_message)
             else:
                 error_message = 'City Already exists'
-                print(error_message)
+                # print(error_message)
+        if error_message:
+            message = error_message
+            message_class = 'alert-danger'
+            print(message)
+        else:
+            message = 'City Added Successfully'
+            message_class = 'alert-success'
+            print(message)
 
-        return HttpResponseRedirect("/")
-    else:
-        # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=19e314a02c78c1106c28037fcf77f454'
-        data_city = City.objects.all()
-        weather_data_list = []
-        for city in data_city:
-            # city = 'Dubai'
-            r = requests.get(url.format(city)).json()
-            # p = requests.get(url.format(city))
-            # r=url.format(city)
-            # print(p.text)
-            weather_data = {'city': city,
-                            'temperature': r['main']['temp'],
-                            'degreecelsius': round((r['main']['temp'] - 32) * 5 / 9, 2),
-                            'humidity': r['main']['humidity'],
-                            'description': r['weather'][0]['description'],
-                            'icon': r['weather'][0]['icon'],
-                            }
-            weather_data_list.append(weather_data)
-        # print(weather_data_list)
-        return render(request, 'index.html', {'weather_data': weather_data_list, 'form': form})
+        # return HttpResponseRedirect("/")
+    # else:
+    # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=19e314a02c78c1106c28037fcf77f454'
+    # new_city = ''
+    form = CityCreateForm()
+    data_city = City.objects.all()
+    weather_data_list = []
+    for city in data_city:
+        # city = 'Dubai'
+        r = requests.get(url.format(city)).json()
+        p = requests.get(url.format(city))
+        # r=url.format(city)
+        print(p.dt)
+        weather_data = {'city': city,
+                        'temperature': r['main']['temp'],
+                        'degreecelsius': round((r['main']['temp'] - 32) * 5 / 9, 2),
+                        'humidity': r['main']['humidity'],
+                        'description': r['weather'][0]['description'],
+                        'icon': r['weather'][0]['icon'],
+                        }
+        weather_data_list.append(weather_data)
+
+    # print(weather_data_list)
+    return render(request, 'index.html', {'weather_data': weather_data_list,
+                                          'form': form,
+                                          'message': message,
+                                          'message_class': message_class
+                                          })
 
 
 def delete(request, city_name):
